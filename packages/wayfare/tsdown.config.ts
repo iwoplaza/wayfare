e16e71c08b1a6780e8788a448131a6f80133bebb
Iwo Plaza <iwoplaza@gmail.com>
2025-07-21 12:35:21 +0200

    Use tsdown

---
+++ packages/wayfare/tsdown.config.ts
@@ -0,0 +1,38 @@
+// import typegpu from 'unplugin-typegpu/rolldown';
+import { defineConfig } from 'tsdown';
+import AutoImport from 'unplugin-auto-import/rollup';
+
+export default defineConfig({
+  platform: 'neutral',
+  dts: true,
+  plugins: [
+    AutoImport({
+      imports: [
+        {
+          typegpu: ['tgpu'],
+          'typegpu/data': [
+            'builtin',
+            'struct',
+            'f32',
+            'i32',
+            'u32',
+            'vec2f',
+            'vec3f',
+            'vec4f',
+            'mat4x4f',
+            ['*', 'd'],
+          ],
+          'typegpu/std': [
+            'mul',
+            'add',
+            'dot',
+            'max',
+            'normalize',
+            ['*', 'std'],
+          ],
+        },
+      ],
+    }),
+    // typegpu({}),
+  ],
+});
