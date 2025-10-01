3e4ea1fa0cf33d10260d790e2252c7792c585058
Iwo Plaza <iwoplaza@gmail.com>
2025-10-01 17:20:44 +0200

    Update index.ts

--- packages/wayfare/src/renderer/index.ts
+++ packages/wayfare/src/renderer/index.ts
@@ -1,6 +1,5 @@
 export { Renderer } from './renderer.ts';
 export type { GameObject } from './renderer.ts';
 export { createRectangleMesh } from './rectangle.ts';
-export { createBoxMesh } from './box.ts';
 export { MaterialTrait, createMaterial } from './material.ts';
 export { BlinnPhongMaterial } from './blinn-phong-material.ts';
